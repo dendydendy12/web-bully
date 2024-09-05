d014e1444fdb123175e0666a685cba9c5da5675d
dendydendy12 <d2079117@gmail.com>
2024-09-05 11:37:32 +0700

    update

--- resources/views/front/category.blade.php
+++ resources/views/front/category.blade.php
@@ -12,7 +12,7 @@
     <article>
         <section id="Category-result" class="max-w-[1130px] mx-auto flex items-center flex-col gap-[30px] mt-[70px]">
             <h1 class="text-4xl leading-[45px] font-bold text-center">
-                Jelajahi Persoalan <br />
+                J <br />
                 {{ $category->name }}
             </h1>
             @if ($category->slug == 'video')
